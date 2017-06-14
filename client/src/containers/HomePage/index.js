2c92746eb7327ff791d72654ed888227aeb412df
Michael Pontus <m.pontus@gmail.com>
2017-06-14 15:15:33 +0300

    Better styling for home page

--- client/src/containers/HomePage/index.js
+++ client/src/containers/HomePage/index.js
@@ -3,6 +3,10 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import PersonIcon from 'material-ui/svg-icons/social/person';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import AddIcon from 'material-ui/svg-icons/av/playlist-add';
+import { grey400 } from 'material-ui/styles/colors';
 
 import {
     makeGetPolls,
@@ -10,6 +14,11 @@ import {
 } from './selectors';
 
 import { homePageVisitted } from './actions';
+import {
+    Avatar, Card, CardText, Divider, FlatButton, IconButton, IconMenu, List, ListItem, MenuItem, RaisedButton,
+    Subheader,
+    TextField,
+} from 'material-ui';
 
 const propTypes = {
     homePageVisitted: PropTypes.func.isRequired,
@@ -34,15 +43,60 @@ class HomePage extends Component {
         }
 
         return (
-            <ul>
-                {polls.map((poll) => (
-                    <li key={poll.get('id')}>
-                        <Link to={`/poll/${poll.get('id')}`}>
-                            {poll.get('title')}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+            <div>
+                <Card>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <Subheader inset style={{ width: 'auto', flex: '1' }}>Recent polls</Subheader>
+                        <div>
+                            <FlatButton
+                                secondary
+                                label="Create New Poll"
+                                icon={<AddIcon/>}
+                                containerElement={<Link to="/new" />}
+                            />
+                        </div>
+                    </div>
+                    <List>
+                        {polls.map((poll) => {
+                            const { id, title, author } = poll.toJS();
+                            const avatar = author.anonymous ? (
+                                <Avatar icon={<PersonIcon />} />
+                            ) : (
+                                <Avatar src={author.avatar} />
+                            );
+                            const username = author.anonymous ? 'Anonymous' : author.username;
+                            const iconButtonElement = (
+                                <IconButton
+                                    touch={true}
+                                    tooltip="more"
+                                    tooltipPosition="bottom-left"
+                                >
+                                    <MoreVertIcon color={grey400} />
+                                </IconButton>
+                            );
+
+                            const rightIconMenu = (
+                                <IconMenu iconButtonElement={iconButtonElement}>
+                                    <MenuItem>Reply</MenuItem>
+                                    <MenuItem>Forward</MenuItem>
+                                    <MenuItem>Delete</MenuItem>
+                                </IconMenu>
+                            );
+
+                            return (
+                                <ListItem
+                                    key={id}
+                                    containerElement={<Link to={`/poll/${id}`}/>}
+                                    leftAvatar={avatar}
+                                    rightIconButton={rightIconMenu}
+                                    primaryText={poll.get('title')}
+                                    secondaryText={`By ${username}`}
+                                />
+                            );
+                        })}
+                    </List>
+                </Card>
+            </div>
         )
     }
 }
